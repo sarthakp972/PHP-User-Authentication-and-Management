e63888bd03e7b9777b34d03e216a4e95d63232ab
sarthakp972 <143208467+sarthakp972@users.noreply.github.com>
2024-03-03 16:56:18 +0530

    A

--- sign_up/index.php
+++ sign_up/index.php
@@ -14,15 +14,15 @@
 <body>
     
 
- <div class="container-fluid d-flex align-items-center justify-content-center vh-100">
+ <div class="container-fluid d-flex align-items-center justify-content-center vh-100  bg-danger-subtle text-danger-emphasis">
            <div class="card">
-                <div class="card-body">
+                <div class="card-body bg-info-subtle">
 
-                    <h1 class="text-center mb-3 ">SignUP Form</h1>
+                    <h1 class="text-center mb-3 bg-info-subtle " id="sign">SignUP Form</h1>
                         <div class="text-center">
                             <!-- 1 page -->
 
-                            <div id="phase1">
+                            <div id="phase1" class="bg-info-subtle">
 
 <div class="flex d-flex justify-content-start mt-2">
       <h5 >Basic Info:</h5>
@@ -32,21 +32,21 @@
   <div class="flex justify-content-start">
     <label for="name" class="form-label">Name</label>
 </div>
-    <input type="text"  class="form-control" placeholder="name greater than 3 char" name="name" id="name">
+    <input type="text"  class="form-control bg-danger-subtle" placeholder="name greater than 3 char" name="name" id="name">
 
 </div>
 
   <div class="mb-3">
     <label for="email" class="form-label" >Email</label>
-    <input type="email" class="form-control" placeholder="example123@gmail.com"   name="email" id="email">
+    <input type="email" class="form-control bg-danger-subtle" placeholder="example123@gmail.com"   name="email" id="email">
   </div>
 
   <div class="mb-3">
-    <label for="phone" class="form-label" >Phone</label>
-    <input type="number" placeholder=" 10 digit phone number " class="form-control" name="phone" id="phone">
+    <label for="phone" class="form-label " >Phone</label>
+    <input type="number" placeholder=" 10 digit phone number " class="form-control bg-danger-subtle" name="phone" id="phone">
   </div>
  
-  <button  onclick="nextPhase()" id="hello" class="btn btn-primary ">Next</button>
+  <button  onclick="nextPhase()" id="hello" class=" btn btn-primary ">Next</button>
   </div>
 
                             <!-- end 1 page -->
@@ -65,7 +65,7 @@
   <label for="gender">Gender:</label>
 </div>
 
-                <select class="form-control" id="gender" name="gender" required>
+                <select class="form-control bg-danger-subtle" id="gender" name="gender" required>
                     <option value="">Select Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
@@ -76,12 +76,12 @@
 
   <div class="mb-3">
     <label for="password" class="form-label" >Password</label>
-    <input type="password" class="form-control" name="password" id="password">
+    <input type="password" placeholder="Insert Password" class="form-control bg-danger-subtle" name="password" id="password">
   </div>
 
   <div class="mb-3">
     <label for="C_password" class="form-label" >Confirm Password</label>
-    <input type="password" class="form-control" name="C_Password" id="C_password">
+    <input type="password" placeholder="Insert Confirm Password" class="form-control bg-danger-subtle" name="C_Password" id="C_password">
   </div>
 <!-- Check -->
   <div class="col-12">
@@ -176,6 +176,7 @@ else if( index1==-1||index2==-1){
 function previous(){
     $("#phase1").show();
     $("#phase2").hide();
+    $("#phase3").hide();
 }
 
 
@@ -218,17 +219,19 @@ console.log(password);
     success: function(response) {
       console.log(response)
     
-        alert("Your form is submitted")
+    
         alert("Thank you!");
-
+        alert(response);
        ///////////////////////////////////////////////////////////////// for phase3
-       alert(response);
+     
        $(document).ready(function() {
-        // Assuming you have a variable named 'your_name'
+       
         var your_name = name1;
         
-        // Use .text() method to set text content of <p> tag
+       
         $("#your_name").text(your_name);
+        $("#sign").text("Profile");
+    
     });
     $("#phase1").hide();
     $("#phase3").show();
